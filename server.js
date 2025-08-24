078b137c73d6658e2a05ffacca4447f485a7aa2a
Andres21y <andres17vh@gmail.com>
2025-08-24 09:09:41 -0500

    fixing app.js

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
-import express from 'express';
+const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+
+// No necesitas la variable 'port' aquí para Vercel
 
 // Servir archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
+// Manejar la ruta raíz para servir el index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
+// La llamada a app.listen() se elimina. Vercel se encarga de esto.
+// app.listen(port, () => {
+//   console.log(`Servidor corriendo en http://localhost:${port}`);
+// });
+
+// Exportar la app para que Vercel la pueda usar
+module.exports = app;
